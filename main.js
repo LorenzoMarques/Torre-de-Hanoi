9e5261cfa4f228ae731d55e36e54c0e1d3bf29ad
Lorenzo Marques <lorenzo.marques246@gmail.com>
2021-11-29 16:14:32 -0300

    att

---
+++ main.js
@@ -0,0 +1,37 @@
+let jogo = document.getElementById("jogo")
+let torre1 = document.createElement("div")
+let torre2 = document.createElement("div")
+let torre3 = document.createElement("div")
+torre1.id = "torre1"
+torre2.id = "torre2"
+torre3.id = "torre3"
+
+jogo.appendChild(torre1)
+jogo.appendChild(torre2)
+jogo.appendChild(torre3)
+
+
+
+
+let disco1 = document.createElement("div")
+let disco2 = document.createElement("div")
+let disco3 = document.createElement("div")
+disco1.id = "disco1"
+disco2.id = "disco2"
+disco3.id = "disco3"
+
+
+//torre 1
+torre1.appendChild(disco1)
+torre1.appendChild(disco2)
+torre1.appendChild(disco3)
+
+//torre 1
+
+//torre 2
+//torre 2
+
+
+//torre 3
+//torre 3
+
